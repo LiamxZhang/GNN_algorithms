640b6f075f3e02cad2356da07bb223f51eb180ff
LiamxZhang <218019067@link.cuhk.edu.cn>
2026-02-06 21:53:04 +0800

    Added requirements.txt and pyproject.toml.

--- GCNLayer.py
+++ GCNLayer.py
@@ -19,9 +19,34 @@ class GCNLayer(nn.Module):
         :param adj_matrix: 邻接矩阵：[batch_size,num_nodes,num_nodes]
         :return:
         """
-        num_neighbors = adj_matrix.sum(dim=-1,keepdims=True)#各节点的邻居数
-        node_feats = self.projection(node_feats)#将特征转化为消息
+        num_neighbors = adj_matrix.sum(dim=-1,keepdims=True) #各节点的邻居数
+        node_feats = self.projection(node_feats)# 将特征转化为消息
         #各邻居节点消息求和并求平均
         node_feats = torch.bmm(adj_matrix,node_feats)
         node_feats = node_feats / num_neighbors
+        
         return node_feats
+    
+if __name__ == "__main__":
+    node_feats = torch.arange(8,
+    dtype=torch.float32).view(1,4,2)
+    adj_matrix = torch.Tensor([[[1,1,0,0],
+                [1,1,1,1],
+                [0,1,1,1],
+                [0,1,1,1]]])
+    print("节点特征：\n",node_feats)
+    print("添加自链接的邻接矩阵：\n",adj_matrix)
+
+    layer = GCNLayer(c_in=2, c_out=2)
+    # 初始化权重矩阵
+    layer.projection.weight.data = torch.Tensor([[1., 0.], [0., 1.]])
+    layer.projection.bias.data = torch.Tensor([0., 0.])
+    
+    # 将节点特征和添加自连接的邻接矩阵输入 GCN 层
+    with torch.no_grad():
+        out_feats = layer(node_feats, adj_matrix)
+    
+    print("节点特征:\n", node_feats)
+    print("添加自连接的邻接矩阵:\n", adj_matrix)
+    print("节点输出特征:\n", out_feats)
+
--- README.md
+++ README.md
@@ -3,11 +3,30 @@ An implementation for torch-based GNN algorithms for task planning tasks.
 
 ## Prerequisites
 
-Anaconda or Miniconda installed on your system
+    Operating System: Ubuntu 22.04
+    NVIDIA Driver: Version 535.288.01 or higher
+    CUDA Version: 12.1
+    Anaconda/Miniconda: Installed on your system
 
-## Step 1: Create Conda Environment
+## Installation
 
+### Step 1: Clone this Project
+Download the project code
+```python
+git clone https://github.com/LiamxZhang/GNN_algorithms.git
+cd GNN_algorithms
+```
+
+### Step 2: Create Conda Environment
+Create a new conda environment with Python 3.10
 ```python
 conda create -n gnn_env python=3.10
 conda activate gnn_env
+```
+
+### Step 3: Install dependencies
+Install all dependencies through requirements.txt file
+
+```python
+pip install -r requirements.txt
 ```
